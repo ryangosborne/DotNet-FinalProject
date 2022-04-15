d1694ee7b5b9b695b64e054b374bc43a4fbf23a7
apollo <apollo@DESKTOP-UPD06S5>
2022-04-15 20:48:51 -0300

    Building out the models and viewmodels

---
+++ NHL_Score_App/Models/GameModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHL_Score_App.Models
+{
+    public class GameModel
+    {
+        // All the fields we need to build out for the API
+    }
+}
